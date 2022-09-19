f90dd89f426b5452d6e81d02ad7f3d6d61a15ae3
VNCS005 <113910198+VNCS005@users.noreply.github.com>
2022-09-19 10:28:33 -0300

    Update webapp00.py

--- webapp00.py
+++ webapp00.py
@@ -4,13 +4,13 @@
 import streamlit as st
   
 # Use st.title("") para adicionar um TÍTULO ao seu Web app
-st.title("Meu prof. é Incrível!")
+st.title("SALVE O CORINTHIANS")
 
 # Use st.header("") para adicionar um CABEÇALHO ao seu Web app
-st.header("Cabeçalho")
+st.header("O CAMPEÃO DOS CAMPEÃO DOS CAMPEÕES")
 
 # Use st.subheader("") para adicionar um SUB CABEÇALHO ao seu Web app
-st.subheader("Sub Cabeçalho")
+st.subheader("ETERNAMENTE, DENTRO DE NOSSOS CORAÇÕES")
 
 # Use st.write("") para adicionar um texto ao seu Web app
-st.write("Como já deve ter percebido, o método st.write() é usado para escrita de texto e informações gerais!")
+st.write("RUMO AO TETRA DA COPA DO BRASIL, O FANTÁSTICO CORINGÃO SE MOSTRA CADA VEZ MAIS FORTE. SERÁ MESMO QUE OS NOSSOS REIZINHOS DO PARQUE CONSEGUIRÃO TAL FEITO?")
